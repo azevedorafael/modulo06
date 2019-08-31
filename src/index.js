59c8e38f43ab8aa9c2a37c609ff9f91f138701aa
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-08-30 22:50:23 -0300

    Add react-navigation

--- src/index.js
+++ src/index.js
@@ -1,57 +1,11 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { View } from 'react-native';
 
 import './config/ReactotronConfig';
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+import Routes from './routes';
 
 const App = () => {
-  return (
-    <View style={styles.sectionContainer}>
-      <Text style={styles.sectionDescription}>
-        Read the docs to discover what to do next:
-      </Text>
-    </View>
-  );
+  return <Routes />;
 };
 
 export default App;
