21148f23a78c47bd11b90cc88a1c15879e51e4c1
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-09-01 17:58:27 -0300

    Get starred from user

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
+import api from '../../services/api';
 
-export default function User({ navigation }) {
-  console.tron.log(navigation.getParam('user'));
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
 
-  return <View />;
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    starts: [],
+  };
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    const { stars } = this.state;
+
+    return <View />;
+  }
 }
-
-User.navigationOptions = {
-  title: 'Users',
-};
